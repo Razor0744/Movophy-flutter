346d020a093884590fbc05fad62bc4183e721d9c
siarheihaurylau <sgavrilov387@gmail.com>
2024-08-29 10:32:11 +0300

    fixed name of const variables

--- lib/features/home_screen/domain/useCase/get_home_data_use_case.dart
+++ lib/features/home_screen/domain/useCase/get_home_data_use_case.dart
@@ -2,14 +2,14 @@ import 'package:test_for_max/features/home_screen/domain/models/home_state.dart'
 import 'package:test_for_max/features/home_screen/domain/repository/anilibria_repository.dart';
 import 'package:test_for_max/features/home_screen/domain/repository/kinopoisk_repository.dart';
 
-const String HORRORS = 'Ужасы';
-const String COMEDY = 'Комедия';
-const String DRAMA = 'Драма';
-const String MELODRAMA = 'Мелодрама';
-const String HORRORS_CATEGORY = '+ужасы';
-const String COMEDY_CATEGORY = 'комедия';
-const String DRAMA_CATEGORY = 'драма';
-const String MELODRAMA_CATEGORY = '!мелодрама';
+const String horrors = 'Ужасы';
+const String comedy = 'Комедия';
+const String drama = 'Драма';
+const String melodrama = 'Мелодрама';
+const String horrorsCategory = '+ужасы';
+const String comedyCategory = 'комедия';
+const String dramaCategory = 'драма';
+const String melodramaCategory = '!мелодрама';
 
 class GetHomeDataUseCase {
   AnilibriaRepository anilibriaRepository;
@@ -20,14 +20,14 @@ class GetHomeDataUseCase {
   Future<HomeState> execute() async {
     final animeSeriesList = await anilibriaRepository.getAnimeSeriesList();
 
-    final horrorList = await kinopoiskRepository.getKinopoiskSeriesList(HORRORS_CATEGORY);
-    final comedyList = await kinopoiskRepository.getKinopoiskSeriesList(COMEDY_CATEGORY);
-    final dramaList = await kinopoiskRepository.getKinopoiskSeriesList(DRAMA_CATEGORY);
-    final melodramaList = await kinopoiskRepository.getKinopoiskSeriesList(MELODRAMA_CATEGORY);
+    final horrorList = await kinopoiskRepository.getKinopoiskSeriesList(horrorsCategory);
+    final comedyList = await kinopoiskRepository.getKinopoiskSeriesList(comedyCategory);
+    final dramaList = await kinopoiskRepository.getKinopoiskSeriesList(dramaCategory);
+    final melodramaList = await kinopoiskRepository.getKinopoiskSeriesList(melodramaCategory);
 
     return HomeState(
         animeSeriesList: animeSeriesList,
         kinopoiskSeriesList: [horrorList, comedyList, dramaList, melodramaList],
-        kinopoiskCategoryList: [HORRORS, COMEDY, DRAMA, MELODRAMA]);
+        kinopoiskCategoryList: [horrors, comedy, drama, melodrama]);
   }
 }
